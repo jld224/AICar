2c7c929405dcd5c5ad475fa16988542166995663
dorito200 <jdarida9@gmail.com>
2023-11-15 20:39:05 -0500

    update

--- P3_HMM_Part1_Template.py
+++ P3_HMM_Part1_Template.py
@@ -19,53 +19,37 @@ def printGrid(grid):
         print(f"{grid[i][grid.shape[1] - 1]:.3f}")
 
 
-def distance(agent_position, car_position, grid_size):
-    """
-    Function that calculates the periodic distance from agent to car.
-    """
-    diff = np.abs(np.array(agent_position) - np.array(car_position))
-    dist = np.minimum(diff, grid_size - diff)
-    return np.sqrt((dist ** 2).sum())
-
-
-
 # Function: Get Belief
 # Updates beliefs based on the distance observation and your car's (agent's) position.
 # Returns your belief of the probability that the hidden car is in each tile. Your
 # belief probabilities should sum to 1.
-def getBelief(df, gridSize, carLength):
-    # standard deviation for the Gaussian distribution
+def getBelief(data, gridSize, carLength):
     std = carLength * 2. / 3
-    # initialize the belief map - all grid cells have equal beliefs initially
-    carPosMap = np.ones((gridSize, gridSize)) / (gridSize ** 2)
+    carPosMap = np.ones((gridSize, gridSize))  # Initialize beliefs as 1 to prevent zero probabilities.
 
-    # iterate over all observations in the data
-    for _, row in df.iterrows():
-        agent_position = (row["agentX"], row["agentY"])
-        eDist = row["eDist"]
+    for index, row in data.iterrows():
+        agentX = row['agentX']
+        agentY = row['agentY']
+        eDist = row['eDist']
 
-        # update belief of each grid cell
-        for i in range(gridSize):
-            for j in range(gridSize):
-                car_position = (i, j)
-                # calculate real distance
-                true_distance = distance(agent_position, car_position, gridSize)
-                # calculate emission probability
-                emission_prob = norm.pdf(eDist, true_distance, std)
-                # update belief
-                carPosMap[i, j] *= emission_prob
+        for x in range(gridSize):
+            for y in range(gridSize):
+                dx, dy = abs(agentX - x), abs(agentY - y)
+                dx, dy = min(dx, gridSize - dx), min(dy, gridSize - dy)
+                d = math.sqrt(dx ** 2 + dy ** 2)
+                pdf = norm.pdf(eDist, loc=d, scale=std)
+                carPosMap[x, y] *= pdf
 
-        # normalize beliefs so they sum to 1
-        tot_belief = carPosMap.sum()
-        carPosMap /= tot_belief
+        # Normalize (prevent underflow and keep sum = 1)
+        carPosMap /= carPosMap.sum()
 
-    # return grid cell with maximum belief
-    max_index = np.unravel_index(np.argmax(carPosMap, axis=None), carPosMap.shape)
-    rowC, colC = max_index
+    # Find car's most probable location
+    rowC, colC = np.unravel_index(np.argmax(carPosMap), carPosMap.shape)
 
     return rowC, colC, carPosMap
 
 
+# No need to change the main function.
 # No need to change the main function.
 def main():
     # Example command line arguments: 10 3 stationaryCarReading10.csv
@@ -76,7 +60,7 @@ def main():
     print(gridSize, reportingTime, microphoneReadingFileName)
 
     data = pd.read_csv(microphoneReadingFileName, nrows=reportingTime)
-    print(data.head())  # take a peak of your data
+    # print(data.head())  # take a peak of your data
 
     df = pd.DataFrame(data, columns=['agentX', 'agentY', 'eDist'])
     rowC, colC, carPosBelief = getBelief(df, gridSize, carLength)  # return numpy array of probabilities
@@ -89,5 +73,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
